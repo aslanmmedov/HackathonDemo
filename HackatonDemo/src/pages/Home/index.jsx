69f5ab2e20e6b336fa00212226256733e49e25eb
aslanmmedov <aslanzm-azmp202@code.edu.az>
2025-06-24 17:31:10 +0400

    cards price and events section done

--- HackatonDemo/src/pages/Home/index.jsx
+++ HackatonDemo/src/pages/Home/index.jsx
@@ -100,47 +100,42 @@ const Home = () => {
           </div>
       </div>
       </section>
-      <section id="OurPortfolio">
+      <section id="OurEvents">
       <div className="container">
         <div className="head">
           <p>Devoted to wonderful beauty</p>
-          <h1>Flower Pricing</h1>
+          <h1>Events Pricing</h1>
         </div>
         <div className="cardsContainer">
             <div className="row">
               <div className="col-4">
                 <div className="pCard">
-                  <div className="icnheart">
-                    <FaRegHeart />
+                  <div className="priceCard">
+                    <h1>$16</h1><p>per table</p>
                   </div>
-                  <h3>Shop Flowers</h3>
-                  <p>summer sale under 50%</p>
+                  <h3>Birthday Events</h3>
+                  <p>Lorem ipsum dolor sit amet laudem partem perfecto per</p>
                   <button>Shop now</button>
                 </div>
               </div>
               <div className="col-4">
-                <div className="iCard">
-                  <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/b4.jpg" alt="" />
+                <div className="pCard">
+                  <div className="priceCard">
+                    <h1>$31</h1><p>per table</p>
+                  </div>
+                  <h3>Birthday Events</h3>
+                  <p>Lorem ipsum dolor sit amet laudem partem perfecto per</p>
+                  <button>Shop now</button>
                 </div>
               </div>
               <div className="col-4">
-                <div className="iCard">
-                  <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/b2.jpg" alt="" />
-                </div>
-              </div>
-              <div className="col-4">
-                <div className="iCard">
-                  <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/b3.jpg" alt="" />
-                </div>
-              </div>
-              <div className="col-4">
-                <div className="iCard">
-                  <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/b2.jpg" alt="" />
-                </div>
-              </div>
-              <div className="col-4">
-                <div className="iCard">
-                  <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/b3.jpg" alt="" />
+                <div className="pCard">
+                  <div className="priceCard">
+                    <h1>$52</h1><p>per table</p>
+                  </div>
+                  <h3>Birthday Events</h3>
+                  <p>Lorem ipsum dolor sit amet laudem partem perfecto per</p>
+                  <button>Shop now</button>
                 </div>
               </div>
             </div>

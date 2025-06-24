fe8854d54f31d0945335bd4db45e980e62d4e7ca
aslanmmedov <aslanzm-azmp202@code.edu.az>
2025-06-24 16:59:11 +0400

    commit

--- HackatonDemo/src/pages/Home/index.jsx
+++ HackatonDemo/src/pages/Home/index.jsx
@@ -1,30 +1,153 @@
 import React from 'react'
+import { FaRegHeart } from "react-icons/fa";
+
 import "./style.scss"
 const Home = () => {
   return (
    <>
-    <section id="OurPortfolio">
+   <section id="triple">
+      <section id="OurPortfolio">
       <div className="container">
         <div className="head">
           <p>Devoted to wonderful beauty</p>
           <h1>Our Portfolio</h1>
-          <div className="cardsContainer">
+        </div>
+        <div className="cardsContainer">
             <div className="row">
               <div className="col-4">
                 <div className="pCard">
                   <div className="icnheart">
-
+                    <FaRegHeart />
                   </div>
                   <h3>Shop Flowers</h3>
                   <p>summer sale under 50%</p>
                   <button>Shop now</button>
                 </div>
               </div>
+              <div className="col-4">
+                <div className="iCard">
+                  <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/b4.jpg" alt="" />
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="iCard">
+                  <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/b2.jpg" alt="" />
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="iCard">
+                  <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/b3.jpg" alt="" />
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="iCard">
+                  <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/b2.jpg" alt="" />
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="iCard">
+                  <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/b3.jpg" alt="" />
+                </div>
+              </div>
             </div>
           </div>
-        </div>
       </div>
-    </section>
+      </section>
+      <section id="OurPortfolio">
+      <div className="container">
+        <div className="head">
+          <p>Devoted to wonderful beauty</p>
+          <h1>Flower Pricing</h1>
+        </div>
+        <div className="cardsContainer">
+            <div className="row">
+              <div className="col-4">
+                <div className="pCard">
+                  <div className="icnheart">
+                    <FaRegHeart />
+                  </div>
+                  <h3>Shop Flowers</h3>
+                  <p>summer sale under 50%</p>
+                  <button>Shop now</button>
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="iCard">
+                  <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/b4.jpg" alt="" />
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="iCard">
+                  <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/b2.jpg" alt="" />
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="iCard">
+                  <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/b3.jpg" alt="" />
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="iCard">
+                  <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/b2.jpg" alt="" />
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="iCard">
+                  <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/b3.jpg" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+      </section>
+      <section id="OurPortfolio">
+      <div className="container">
+        <div className="head">
+          <p>Devoted to wonderful beauty</p>
+          <h1>Flower Pricing</h1>
+        </div>
+        <div className="cardsContainer">
+            <div className="row">
+              <div className="col-4">
+                <div className="pCard">
+                  <div className="icnheart">
+                    <FaRegHeart />
+                  </div>
+                  <h3>Shop Flowers</h3>
+                  <p>summer sale under 50%</p>
+                  <button>Shop now</button>
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="iCard">
+                  <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/b4.jpg" alt="" />
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="iCard">
+                  <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/b2.jpg" alt="" />
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="iCard">
+                  <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/b3.jpg" alt="" />
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="iCard">
+                  <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/b2.jpg" alt="" />
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="iCard">
+                  <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/b3.jpg" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+      </section>
+   </section>
    </>
   )
 }

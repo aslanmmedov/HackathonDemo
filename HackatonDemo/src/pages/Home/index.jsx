3f648ca3eea83974c0a120debcebeb9e817f2344
aslanmmedov <aslanzm-azmp202@code.edu.az>
2025-06-24 16:06:35 +0400

    some changes

--- HackatonDemo/src/pages/Home/index.jsx
+++ HackatonDemo/src/pages/Home/index.jsx
@@ -3,7 +3,17 @@ import "./style.scss"
 const Home = () => {
   return (
    <>
-   
+    <section id="OurPortfolio">
+      <div className="container">
+        <div className="head">
+          <p>Devoted to wonderful beauty</p>
+          <h1>Our Portfolio</h1>
+          <div className="cards">
+            
+          </div>
+        </div>
+      </div>
+    </section>
    </>
   )
 }

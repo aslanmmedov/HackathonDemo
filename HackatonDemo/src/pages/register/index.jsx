9722380b522245e0f52b556f4b51e2a427071034
ClassTime <classtime@code.local>
2025-06-24 17:30:18 +0400

    register

--- HackatonDemo/src/pages/register/index.jsx
+++ HackatonDemo/src/pages/register/index.jsx
@@ -1,9 +1,108 @@
 import React from 'react'
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
-const Register = () => {
+
+function Register() {
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      userName: '',
+      email: '',
+      password:'',
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .max(15, 'Must be 15 characters or less')
+        .required('Required'),
+        userName: Yup.string()
+        .max(15, 'Must be 15 characters or less')
+        .required('Required'),
+      lastName: Yup.string()
+        .max(20, 'Must be 20 characters or less')
+        .required('Required'),
+      email: Yup.string().email('Invalid email address').required('Required'),
+      password: Yup.string()
+      .min(6, "Password must be at least 6 characters")
+      .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .matches(/[a-z]/, "Password must contain at least one lowercase letter")
+      .matches(/\d/, "Password must contain at least one number")
+      .required("Required"),
+    }),
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
-    <div>Register</div>
-  )
+
+
+   <div>
+      <form onSubmit={formik.handleSubmit}>
+       <input
+         id="firstName"
+         name="firstName"
+         type="text"
+         onChange={formik.handleChange}
+         onBlur={formik.handleBlur}
+         value={formik.values.firstName}
+       />
+       {formik.touched.firstName && formik.errors.firstName ? (
+         <div>{formik.errors.firstName}</div>
+       ) : null}
+ 
+       <input
+         id="lastName"
+         name="lastName"
+         type="text"
+         onChange={formik.handleChange}
+         onBlur={formik.handleBlur}
+         value={formik.values.lastName}
+       />
+       {formik.touched.lastName && formik.errors.lastName ? (
+         <div>{formik.errors.lastName}</div>
+       ) : null}
+ 
+ <input
+         id="userName"
+         name="userName"
+         type="text"
+         onChange={formik.handleChange}
+         onBlur={formik.handleBlur}
+         value={formik.values.userName}
+       />
+       {formik.touched.userName && formik.errors.userName ? (
+         <div>{formik.errors.userName}</div>
+       ) : null}
+ 
+       <input
+         id="email"
+         name="email"
+         type="email"
+         onChange={formik.handleChange}
+         onBlur={formik.handleBlur}
+         value={formik.values.email}
+       />
+       {formik.touched.email && formik.errors.email ? (
+         <div>{formik.errors.email}</div>
+       ) : null}
+        <input
+         id="password"
+         name="password"
+         type="password"
+         onChange={formik.handleChange}
+         onBlur={formik.handleBlur}
+         value={formik.values.password}
+       />
+       {formik.touched.password && formik.errors.password ? (
+         <div>{formik.errors.password}</div>
+       ) : null}
+ 
+       <button type="submit">Submit</button>
+     </form>
+   </div>
+
+  );
 }
 
 export default Register

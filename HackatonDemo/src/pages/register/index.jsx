5dd98140920456ddf09553408a46b3b62ed9e4cf
Ebru18 <ebrubendeliyeva@gmail.com>
2025-06-25 16:11:09 +0400

    login/register completed

--- HackatonDemo/src/pages/register/index.jsx
+++ HackatonDemo/src/pages/register/index.jsx
@@ -1,106 +1,140 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
+import './index.scss';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthContex';
+import { BASE_URL } from '../../Api/constants';
 
-
+console.log(BASE_URL);
 function Register() {
+
+  const { register } = useContext(AuthContext);
+// console.log(register);
   const formik = useFormik({
     initialValues: {
-      firstName: '',
-      lastName: '',
-      userName: '',
+      name: '',
+      surname: '',
+      username: '',
       email: '',
-      password:'',
+      password: '',
+      passwordConfirm: '',
     },
     validationSchema: Yup.object({
-      firstName: Yup.string()
+      name: Yup.string()
         .max(15, 'Must be 15 characters or less')
         .required('Required'),
-        userName: Yup.string()
+      username: Yup.string()
         .max(15, 'Must be 15 characters or less')
         .required('Required'),
-      lastName: Yup.string()
+      surname: Yup.string()
         .max(20, 'Must be 20 characters or less')
         .required('Required'),
       email: Yup.string().email('Invalid email address').required('Required'),
       password: Yup.string()
-      .min(6, "Password must be at least 6 characters")
-      .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
-      .matches(/[a-z]/, "Password must contain at least one lowercase letter")
-      .matches(/\d/, "Password must contain at least one number")
-      .required("Required"),
+        .min(6, "Password must be at least 6 characters")
+        .required("Required"),
+      passwordConfirm: Yup.string()
+        .oneOf([Yup.ref('password'), null], 'Passwords must match')
+        .required('Required'),
     }),
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
+    onSubmit: async (values) => {
+      console.log(values);
+      await register(values);
+      console.log("Registering user...");
     },
   });
+
+
+
+
   return (
 
 
-   <div>
+    <div className='Auth'>
+      <h2>Register</h2>
       <form onSubmit={formik.handleSubmit}>
-       <input
-         id="firstName"
-         name="firstName"
-         type="text"
-         onChange={formik.handleChange}
-         onBlur={formik.handleBlur}
-         value={formik.values.firstName}
-       />
-       {formik.touched.firstName && formik.errors.firstName ? (
-         <div>{formik.errors.firstName}</div>
-       ) : null}
- 
-       <input
-         id="lastName"
-         name="lastName"
-         type="text"
-         onChange={formik.handleChange}
-         onBlur={formik.handleBlur}
-         value={formik.values.lastName}
-       />
-       {formik.touched.lastName && formik.errors.lastName ? (
-         <div>{formik.errors.lastName}</div>
-       ) : null}
- 
- <input
-         id="userName"
-         name="userName"
-         type="text"
-         onChange={formik.handleChange}
-         onBlur={formik.handleBlur}
-         value={formik.values.userName}
-       />
-       {formik.touched.userName && formik.errors.userName ? (
-         <div>{formik.errors.userName}</div>
-       ) : null}
- 
-       <input
-         id="email"
-         name="email"
-         type="email"
-         onChange={formik.handleChange}
-         onBlur={formik.handleBlur}
-         value={formik.values.email}
-       />
-       {formik.touched.email && formik.errors.email ? (
-         <div>{formik.errors.email}</div>
-       ) : null}
         <input
-         id="password"
-         name="password"
-         type="password"
-         onChange={formik.handleChange}
-         onBlur={formik.handleBlur}
-         value={formik.values.password}
-       />
-       {formik.touched.password && formik.errors.password ? (
-         <div>{formik.errors.password}</div>
-       ) : null}
- 
-       <button type="submit">Submit</button>
-     </form>
-   </div>
+          id="name"
+          name="name"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.name}
+          placeholder='Name'
+        />
+        {formik.touched.name && formik.errors.name ? (
+          <div>{formik.errors.name}</div>
+        ) : null}
+
+        <input
+          id="surname"
+          name="surname"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.surname}
+          placeholder='Surname'
+        />
+        {formik.touched.surname && formik.errors.surname ? (
+          <div>{formik.errors.surname}</div>
+        ) : null}
+
+        <input
+          id="username"
+          name="username"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.username}
+          placeholder='User Name'
+        />
+        {formik.touched.username && formik.errors.username ? (
+          <div>{formik.errors.username}</div>
+        ) : null}
+
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+          placeholder='Email'
+        />
+        {formik.touched.email && formik.errors.email ? (
+          <div>{formik.errors.email}</div>
+        ) : null}
+        <input
+          id="password"
+          name="password"
+          type="password"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.password}
+          placeholder='Password'
+        />
+        {formik.touched.password && formik.errors.password ? (
+          <div>{formik.errors.password}</div>
+        ) : null}
+
+  <input
+          id="passwordConfirm"
+          name="passwordConfirm"
+          type="password"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.passwordConfirm}
+          placeholder='Confirm Password'
+        />
+        {formik.touched.passwordConfirm && formik.errors.passwordConfirm ? (
+          <div>{formik.errors.passwordConfirm}</div>
+        ) : null}
+
+        <button type="submit">Submit</button>
+      </form>
+      <div>Already have an account? <Link to="/login">Login</Link></div>
+    </div>
 
   );
 }
